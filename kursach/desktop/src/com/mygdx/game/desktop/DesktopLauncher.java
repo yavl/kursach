efc8cac7776a78201fcaf88c8fe1782d87d8dfd1
Grestomaniac <36094797+Grestomaniac@users.noreply.github.com>
2019-05-03 16:16:29 +0900

    lwjgl2

--- kursach/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ kursach/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.mygdx.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.kursach.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		new Lwjgl3Application(new Main(), config);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
 	}
 }
