246be2c55d48d9c4c1ac4c19dc6204369fbb1cfe
Vlad <yavl98@mail.ru>
2019-05-06 18:17:14 +0900

    ненужный import

--- kursach/core/src/com/kursach/StageManager.java
+++ kursach/core/src/com/kursach/StageManager.java
@@ -12,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
